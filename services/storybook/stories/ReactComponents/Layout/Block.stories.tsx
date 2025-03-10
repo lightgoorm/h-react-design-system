c5ad812674c41366efbaee7a1b10f393595548c7
lightgoorm <wkhhambatt@gmail.com>
2025-03-10 22:08:18 +0900

    환경 설정 (3) - 스토리북 설정

---
+++ services/storybook/stories/ReactComponents/Layout/Block.stories.tsx
@@ -0,0 +1,14 @@
+import "@hdesignsystem/react-components-layout/style.css";
+// @ts-ignore
+import { Block as Block } from "@hdesignsystem/react-components-layout";
+
+export default{
+  title : "React Components | Layout | Block",
+  component : Block,
+  parameters: {
+    layout : "centered"
+  },
+  tags: ["autodocs"]
+}
+
+export const BlockStory = {};
